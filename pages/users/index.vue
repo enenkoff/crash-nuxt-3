c2b0fc426a73103d27682ff910b35fe538feab33
enenkov <enenkoff@gmail.com>
2020-04-16 16:45:26 +0300

    add token in asyncData

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <h1>{{pageTitle}}</h1>
-    {{token}}
+<!--    {{token}}-->
     <ul>
       <li v-for="user of users" :key="user.id">
         <a href="#" @click.prevent="openUser(user)">
@@ -15,22 +15,37 @@
 
 <script>
 export default {
-  async fetch({store}) {
+  // async fetch({store}) {
+  //
+  //   if (store.getters['users/users'].length === 0) {
+  //     await store.dispatch('users/fetch')
+  //   }
+  // },
+    async asyncData ({ app }) {
 
-    if (store.getters['users/users'].length === 0) {
-      await store.dispatch('users/fetch')
-    }
+      const token = await app.$cookies.get('quwi_user_token')
+
+      let users;
+      await app.$axios.get('https://api.quwi.com/v2/projects', {
+          headers: {
+              Authorization: 'Bearer ' + token
+          }
+      }).then((response) => {
+          users = response.data.projects
+      })
+
+      return { users }
   },
   data: () => ({
     pageTitle: 'Users page'
   }),
   computed: {
-    users() {
-      return this.$store.getters['users/users']
-    },
-    token() {
-      return this.$store.getters['users/token']
-    }
+    // users() {
+    //   return this.$store.getters['users/users']
+    // },
+    // token() {
+    //   return this.$store.getters['users/token']
+    // }
   },
   methods: {
     openUser(user) {
